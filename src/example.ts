6ecc646bec615e88e8e5e406f9d6db6b1abd4224
Piyush <piyush.puniya@telecrm.in>
2024-06-21 21:00:14 +0530

    updated example

--- src/example.ts
+++ src/example.ts
@@ -1,67 +1,67 @@
-// import { RegisterClass, parseJson } from "json-class-npm";
+import { RegisterClass, parseJson } from "json-class-npm";
 
-// @RegisterClass('C')
-// class C {
+@RegisterClass('C')
+class C {
 
-//     constructor(
-//         public propC: string
-//     ) {
+    constructor(
+        public propC: string
+    ) {
 
-//     }
+    }
 
-//     getProp() {
-//         return "inside c " + this.propC
-//     }
-// }
+    getProp() {
+        return "inside c " + this.propC
+    }
+}
 
-// @RegisterClass('B')
-// class B {
-//     constructor(
-//         public propB: number
-//     ) {
+@RegisterClass('B')
+class B {
+    constructor(
+        public propB: number
+    ) {
 
-//     }
-// }
+    }
+}
 
-// @RegisterClass('A')
-// class A {
-//     constructor(
-//         public propA: string,
-//         public objB: B,
-//         public arrC: C[]
-//     ) {
+@RegisterClass('A')
+class A {
+    constructor(
+        public propA: string,
+        public objB: B,
+        public arrC: C[]
+    ) {
 
-//     }
+    }
 
-//     getA() {
-//         return this.propA
-//     }
+    getA() {
+        return this.propA
+    }
 
-//     getB() {
-//         console.log("objB")
-//     }
-// }
+    getB() {
+        console.log("objB")
+    }
+}
 
-// // Example usage
-// const jsonString = {
-//     __class: 'A',
-//     propA: 'Hello',
-//     objB: {
-//         __class: 'B',
-//         propB: 42
-//     },
-//     arrC: [
-//         {
-//             __class: 'C',
-//             propC: 'World'
-//         },
-//         {
-//             __class: 'C',
-//             propC: '!'
-//         }
-//     ]
-// };
+// Example usage
+const jsonString = {
+    __class: 'A',
+    propA: 'Hello',
+    objB: {
+        __class: 'B',
+        propB: 42
+    },
+    arrC: [
+        {
+            __class: 'C',
+            propC: 'World'
+        },
+        {
+            __class: 'C',
+            propC: '!'
+        }
+    ]
+};
 
-// const objA: A = parseJson(jsonString);
-// console.log(objA.arrC[0].getProp())
+const objA: A = parseJson(jsonString);
+console.log(objA.arrC[0].getProp())
 
